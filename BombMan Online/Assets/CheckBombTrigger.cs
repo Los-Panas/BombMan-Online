9dc001dae6ebb2356717f9eda2396f53ce5e26e1
VictorSegura99 <36154682+VictorSegura99@users.noreply.github.com>
2021-01-09 18:52:29 +0100

    Idk anymore I wan't to cry

---
+++ BombMan Online/Assets/CheckBombTrigger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckBombTrigger : MonoBehaviour
+{
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.gameObject.layer = 2;
+            Debug.Log("Okeyy");
+        }
+    }
+}
