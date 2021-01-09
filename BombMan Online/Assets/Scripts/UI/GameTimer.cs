d537e0fe30bd558b606966af3a5599e792992662
didaclis <36189205+didaclis@users.noreply.github.com>
2021-01-09 17:26:10 +0100

    clock

---
+++ BombMan Online/Assets/Scripts/UI/GameTimer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameTimer : MonoBehaviour
+{
+    [SerializeField]
+    private float totalGameTime;
+    private float timeToFinish;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //NewGame();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(timeToFinish - Time.time <= 0)
+        {
+            GetComponent<Text>().text = "0'00 s";
+        }
+        else
+        {
+            double timeseg = timeToFinish - Time.time;
+            timeseg = System.Math.Round(timeseg, 2);
+            GetComponent<Text>().text = timeseg.ToString() + " s";
+        }
+    }
+
+    public void NewGame()
+    {
+        timeToFinish = totalGameTime + Time.time;
+    }
+}
