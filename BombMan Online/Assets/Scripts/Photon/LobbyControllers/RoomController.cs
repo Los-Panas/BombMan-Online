ba9eb1f047298b4729d1a21503e04970c414ef14
didaclis <36189205+didaclis@users.noreply.github.com>
2021-01-09 19:23:52 +0100

    disconnect if is the same name

--- BombMan Online/Assets/Scripts/Photon/LobbyControllers/RoomController.cs
+++ BombMan Online/Assets/Scripts/Photon/LobbyControllers/RoomController.cs
@@ -61,7 +61,7 @@ public class RoomController : MonoBehaviourPunCallbacks, IInRoomCallbacks
     public override void OnLeftRoom()
     {
         base.OnLeftRoom();
-        //PhotonNetwork.LoadLevel(0);
+        PhotonNetwork.LoadLevel(0);
     }
 
     private void StartGame()
@@ -75,7 +75,8 @@ public class RoomController : MonoBehaviourPunCallbacks, IInRoomCallbacks
         currentScene = scene.buildIndex;
         if(currentScene == multiplayerScene)
         {
-            CreatePlayer();
+            if (!CheckIfNameIsAlreadyTaken())
+                CreatePlayer();
         }
     }
 
